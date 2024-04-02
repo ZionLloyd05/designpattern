8f9439872e168f8ce5d745ad1a1d38daeb7d9ce2
Alagbala Damilola <alagbaladamilola@gmail.com>
2024-04-02 12:28:28 +0100

    Add project files.

---
+++ DesignPatterns.API/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.API
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DesignPatterns.Singleton/Singleton.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton? _instance;
+
+        private Singleton() { }
+
+        public static Singleton Create()
+        {
+            _instance ??= new Singleton();
+            return _instance;
+        }
+    }
+}
